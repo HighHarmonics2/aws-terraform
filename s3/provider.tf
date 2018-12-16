e66b7c49e31a0f3396f55dfa9148b723e5560c5e
jure <jure@comcast.net>
2018-12-15 23:06:30 -0800

    Removed S3 folder that was added incorrectly

--- s3/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region                  = "${var.aws-region}"
-  shared_credentials_file = "${var.awsCredsPath}.aws/credentials"
-  profile                 = "jure2"
-}
--- s3/vars.tf
+++
@@ -1,9 +0,0 @@
-
-variable "awsCredsPath" {
-  description = "location of credentials"
-  default     = "/Users/jure/"
-}
-
-variable "aws-region" {
-  default     = "us-west-2"
-}
